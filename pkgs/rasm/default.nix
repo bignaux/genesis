00074001c25f54c489aea15de86fa2da1a9ccaae
Bignaux Ronan <ronan@aimao.org>
2020-11-29 14:39:52 +0100

    rasm: update 1.2 -> 1.3

--- pkgs/rasm/default.nix
+++ pkgs/rasm/default.nix
@@ -2,20 +2,16 @@
 
 stdenv.mkDerivation rec {
   pname = "rasm";
-  version = "1.2";
+  version = "1.3";
 
   src = fetchFromGitHub {
     owner = "EdouardBERGE";
     repo = "rasm";
     rev = "v${version}";
-    sha256 = "1nfmr4s6pk0mpqzgii77pkck9ak09vd9y0vgkxlp64vwlwyb84hc";
+    sha256 = "18q75vkaqpg96kki99zwmgc3px6sr094jzqv91cdd7xr053kyqmv";
   };
 
-  buildPhase = ''
-      # according to official documentation
-      cc rasm.c -O2 -lm -lrt -march=native -o rasm
-  '';
-
+  makeFlags = [ "EXEC=rasm" ];
   installPhase = ''
     install -Dt $out/bin rasm
   '';
