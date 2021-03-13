0e711d8bf42a5861d0e66cdd7bd97a54d75d0ee6
Bignaux Ronan <ronan@aimao.org>
2021-03-13 20:33:09 +0100

    fix

--- pkgs/development/python-modules/default.nix
+++ pkgs/development/python-modules/default.nix
@@ -1,8 +1,7 @@
 { lib, callPackage }:
 
 rec {
-
+  frida = callPackage ./frida.nix { lib = mylib; };
   pycotap = callPackage ./pycotap {};
   pysol_cards = callPackage ./pysol_cards {};
-  frida = callPackage ./frida.nix {};
 }
